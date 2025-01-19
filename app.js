29d68016d01b8a85c0581a24d7e6f71ae51ef99b
Enmanuel <end199615@gmail.com>
2025-01-19 11:31:47 -0500

    crear function agregarAmigo y limpiarEntrada

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+/* El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. 
+Aquí deberás desarrollar la lógica para resolver el problema.*/
+
+// Crear un array para almacenar los nombres
+let amigos = [];
+
+function agregarAmigo() {
+    let amigo = document.getElementById("amigo").value;
+
+    if (amigo == "") {
+        alert("Por favor, inserte un nombre.");
+    } else {
+        amigos.push(amigo);
+        limpiarEntrada();
+    }
+    console.log(amigos);
+}
+
+function limpiarEntrada() {
+    let cajaAmigo = document.getElementById("amigo");
+    cajaAmigo.value = "";
+}
