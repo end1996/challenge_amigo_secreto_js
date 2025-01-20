bf5e36620358796eaef3badf5016f5668743d3a8
Enmanuel <end199615@gmail.com>
2025-01-20 09:51:26 -0500

    crear funcion generarIndice, limpiarLista, asignarTextoElemento y reutilizar en sortearAmigo

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ Aquí deberás desarrollar la lógica para resolver el problema.*/
 
 // Crear un array para almacenar los nombres
 let amigos = [];
+let amigosSorteados = [];
+let listaIndice = [];
 
 function agregarAmigo() {
     let amigo = document.getElementById("amigo").value;
@@ -27,9 +29,8 @@ function limpiarEntrada() {
 }
 
 function actualizarListaAmigos() {
-    let lista = document.getElementById("listaAmigos");
-    lista.innerHTML = "";
-    
+    let lista = limpiarLista();
+
     for (let i = 0; i < amigos.length; i++) {
         let elemento = crearElemento();
         elemento.innerHTML = amigos[i];
@@ -42,4 +43,47 @@ function crearElemento() {
     // crear elemento de la lista
     let elemento = document.createElement("li");
     return elemento;
+}
+
+function sortearAmigo() {
+    // Validar que hay amigos disponibles
+     if (amigos.length > 0) {
+        // Generar un índice aleatorio
+        let indice = generarIndice();
+        if (indice === -1) {
+            return; // No hacer nada si ya se han sorteado todos
+        }
+        // Obtener el nombre sorteado
+        let nombre = amigos[indice];
+        // Mostrar el resultado
+        limpiarLista();
+        asignarTextoElemento("resultado", `El amigo sorteado es: ${nombre}` );
+     } else {
+        alert("No hay amigos para sortear.");
+     }
+}
+
+function limpiarLista() {
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+    return lista;
+}
+
+function generarIndice () {
+    let indice = Math.floor(Math.random() * amigos.length);
+    if (listaIndice.length == amigos.length) {
+        asignarTextoElemento("resultado", "Ya se han sorteado todos los amigos");
+        return -1;
+    }
+    if (listaIndice.includes(indice)) {
+        return generarIndice();
+    } else {
+        listaIndice.push(indice);
+        console.log(listaIndice);
+        return indice;
+    }
+}
+
+function asignarTextoElemento(elemento, texto) {
+    return document.getElementById(elemento).innerHTML = texto;
 }
